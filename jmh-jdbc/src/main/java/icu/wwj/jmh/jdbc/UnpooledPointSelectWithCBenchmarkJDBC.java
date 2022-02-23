98f05ef483982502cf937b6d83c419ac8bde4d5b
吴伟杰 <wuweijie@apache.org>
2022-02-23 11:09:55 +0800

    Refactor jmh-jdbc
    (cherry picked from commit a17619ee7c6f62fe4f367e3fe7f49eacd05f79d5)

---
+++ jmh-jdbc/src/main/java/icu/wwj/jmh/jdbc/UnpooledPointSelectWithCBenchmarkJDBC.java
@@ -0,0 +1,6 @@
+package icu.wwj.jmh.jdbc;
+
+import icu.wwj.jmh.jdbc.connection.UnpooledJDBCConnectionProvider;
+
+public class UnpooledPointSelectWithCBenchmarkJDBC extends UnpooledPointSelectWithCBenchmarkBase implements UnpooledJDBCConnectionProvider {
+}
---
+++ jmh-jdbc/src/main/java/icu/wwj/jmh/jdbc/UnpooledWriteOnlyBenchmarkJDBC.java
@@ -0,0 +1,6 @@
+package icu.wwj.jmh.jdbc;
+
+import icu.wwj.jmh.jdbc.connection.UnpooledJDBCConnectionProvider;
+
+public class UnpooledWriteOnlyBenchmarkJDBC extends UnpooledWriteOnlyBenchmarkBase implements UnpooledJDBCConnectionProvider {
+}
